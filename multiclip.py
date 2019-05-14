7a290a381277103b0df4fdec46952fb427850c96
orrpeles <orr.backup37@gmail.com>
2019-05-14 16:41:59 +0300

    added improvements to multiclip.py

--- multiclip.py
+++ multiclip.py
@@ -10,6 +10,13 @@ import shelve, pyperclip, sys
 mcbShelf = shelve.open('mcb')
 if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
     mcbShelf[sys.argv[2]] = pyperclip.paste()
-# elif len(sys.argv) == 2:
+    print("\n",sys.argv[2]) 
+elif len(sys.argv) == 2:
+    if len(sys.argv) == 2 and sys.argv[1].lower() == 'list':
+        pyperclip.copy(str(list(mcbShelf.keys())))
+        print(pyperclip.paste())
+    elif sys.argv[1] in mcbShelf:
+        pyperclip.copy(mcbShelf[sys.argv[1]])
 mcbShelf.close()
+
 
